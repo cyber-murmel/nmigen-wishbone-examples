ed9a1f6f4b2858067fbbbb5a468d10e4dc5b8185
marble <marble@hamburg.ccc.de>
2020-11-06 15:36:49 +0100

    applied python black

--- output_port/__main__.py
+++ output_port/__main__.py
@@ -6,35 +6,49 @@ from argparse import ArgumentParser
 from logging import debug, info, warning, error, basicConfig as logConfig
 from logging import DEBUG, INFO, WARNING, ERROR
 import random
-
 from .output_port import OuputPort
 
-def parse_arguments():
-    parser = ArgumentParser(
-        description="Python CLI Template",
-        epilog="")
-    
-    verbose = parser.add_mutually_exclusive_group()
-    verbose.add_argument("-q", "--quiet",   action="store_true", help="turn off warnings")
-    verbose.add_argument("-v", "--verbose", action="count",      help="set verbose loglevel")
+widths = (8, 16, 32, 64)
 
-    parser.add_argument("--vcd",         type=str, default="/tmp/traces.vcd",         help="path to vcd file")
-    parser.add_argument("--gtkw",        type=str, default="/tmp/traces.gtkw",        help="path to gtkw file")
-    widths = (8, 16, 32, 64)
-    parser.add_argument('--width',       type=int, choices=widths, default=widths[0], help="bus data width")
-    parser.add_argument('--granularity', type=int, choices=widths,                    help="bus data granularity")
-    parser.add_argument('--loop',        type=int, default=32,                        help="number of test loops to run")
-    
+
+def parse_arguments():
+    parser = ArgumentParser(description="Python CLI Template", epilog="")
+
+    verbose = parser.add_mutually_exclusive_group()
+    verbose.add_argument("-q", "--quiet", action="store_true", help="turn off warnings")
+    verbose.add_argument("-v", "--verbose", action="count", help="set verbose loglevel")
+
+    parser.add_argument(
+        "--vcd", type=str, default="/tmp/traces.vcd", help="path to vcd file"
+    )
+    parser.add_argument(
+        "--gtkw", type=str, default="/tmp/traces.gtkw", help="path to gtkw file"
+    )
+    parser.add_argument(
+        "--width", type=int, choices=widths, default=widths[0], help="bus data width"
+    )
+    parser.add_argument(
+        "--granularity", type=int, choices=widths, help="bus data granularity"
+    )
+    parser.add_argument(
+        "--loop", type=int, default=32, help="number of test loops to run"
+    )
+
     args = parser.parse_args()
     return args
 
+
 def configure_logging(verbose, quiet):
-    log_level = ERROR   if quiet else \
-                WARNING if not verbose else \
-                INFO    if 1 == verbose else \
-                DEBUG   #  2 <= verbose
-    logConfig(format="%(asctime)-15s %(levelname)-8s %(message)s", datefmt="%Y-%m-%d %H:%M:%S", level=log_level)
+    log_level = (
+        ERROR if quiet else WARNING if not verbose else INFO if 1 == verbose else DEBUG
+    )  # 2 <= verbose
+    logConfig(
+        format="%(asctime)-15s %(levelname)-8s %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
+        level=log_level,
+    )
     debug("DEBUG Log Level")
+
 
 def main(args):
     width = args.width
@@ -63,28 +77,31 @@ def main(args):
             yield dut.dat_w.eq(0)
             yield dut.stb.eq(0)
             yield dut.we.eq(0)
-        
+
         # make reproducible
         random.seed(42)
 
         while gran <= width:
-            n_sel_bits = gran//granularity
+            n_sel_bits = gran // granularity
             for _ in range(args.loop):
-                for sel in range(width//gran):
-                    expected = random.randrange(1<<gran)
-                    yield from cycle(expected<<(gran*sel), ((1<<n_sel_bits)-1)<<(sel*n_sel_bits))
+                for sel in range(width // gran):
+                    expected = random.randrange(1 << gran)
+                    yield from cycle(
+                        expected << (gran * sel),
+                        ((1 << n_sel_bits) - 1) << (sel * n_sel_bits),
+                    )
                     # wait for signal propagation through register
                     yield Tick()
                     # compare
                     actual = yield dut.dat_r.word_select(sel, gran)
                     assert actual == expected
-            
+
             gran <<= 1
-    
+
     sim = Simulator(dut)
     with sim.write_vcd(args.vcd, args.gtkw, traces=dut.ports()):
         # Add a clock to our design
-        sim.add_clock(1/25e6)
+        sim.add_clock(1 / 25e6)
         # Add 'process' as a testbench process
         sim.add_sync_process(process)
         # Run the simulation
@@ -92,6 +109,7 @@ def main(args):
 
     info("All tests passed!")
     info("Run `gtkwave {}` to inspect wave forms".format(args.gtkw))
+
 
 if "__main__" == __name__:
     args = parse_arguments()
